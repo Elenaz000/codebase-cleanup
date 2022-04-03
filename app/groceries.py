533dc84b4bbbec986e5347c2596711611771edff
Elenaz000 <yz760@georgetown.edu>
2022-04-03 16:47:58 -0400

    Update groceries.py

--- app/groceries.py
+++ app/groceries.py
@@ -7,20 +7,7 @@
 
 import os
 
-
-def to_usd(my_price):
-    """
-    This is a docstring. It tells us what this function is about.
-    What its responsibilities ar.
-    What the params are ab9out.
-    What datatypes the params are.
-    What this function will return.
-    Example of invoking the function.
-
-    Invoke like this: to_usd(9.9999)
-    """
-    return '${:,.2f}'.format(my_price)
-
+from app.utils import to_usd
 
 # checks to see if a products.csv file exists. If not, it uses the default
 if os.path.isfile(os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")) == True:
