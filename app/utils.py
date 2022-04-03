84f9a91003cfeb02ce2258ce0e09a52f0bd7e936
Elenaz000 <yz760@georgetown.edu>
2022-04-03 16:48:05 -0400

    Create utils.py

---
+++ app/utils.py
@@ -0,0 +1,12 @@
+def to_usd(my_price):
+    """
+    This is a docstring. It tells us what this function is about.
+    What its responsibilities ar.
+    What the params are ab9out.
+    What datatypes the params are.
+    What this function will return.
+    Example of invoking the function.
+
+    Invoke like this: to_usd(9.9999)
+    """
+    return '${:,.2f}'.format(my_price)
