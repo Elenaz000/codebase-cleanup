4cc777a50beb9d18b9b18a15a66caa076802689e
Elenaz000 <yz760@georgetown.edu>
2022-04-05 21:49:52 -0400

    Update crypto.py

--- app/crypto.py
+++ app/crypto.py
@@ -5,21 +5,23 @@ print("CRYPTO REPORT...")
 
 import os
 import json
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import requests
 
 from app.utils import to_usd
+from app.alphavantage_service import fetch_crypto_data
 
 load_dotenv()
 
-ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
-
-symbol = input("Please input a crypto symbol (default: 'BTC'): ") or "BTC"
-url = f"https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&market=USD&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
-response = requests.get(url)
-parsed_response = json.loads(response.text)
+#ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
+#
+#symbol = input("Please input a crypto symbol (default: 'BTC'): ") or "BTC"
+#url = f"https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&market=USD&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+#response = requests.get(url)
+#parsed_response = json.loads(response.text)
 #print(parsed_response)
 #breakpoint()
+parsed_response = fetch_crypto_data(symbol)
 
 tsd = parsed_response["Time Series (Digital Currency Daily)"]
 
