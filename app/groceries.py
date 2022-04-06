9e6a17012193b7e06f0f0468f770aebdb71fe625
Elenaz000 <yz760@georgetown.edu>
2022-04-05 21:49:54 -0400

    Update groceries.py

--- app/groceries.py
+++ app/groceries.py
@@ -8,14 +8,15 @@
 import os
 
 from app.utils import to_usd
+from app.utils import read_csv
 
 # checks to see if a products.csv file exists. If not, it uses the default
-if os.path.isfile(os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")) == True:
+if os.path.isfile(read_csv("products.csv")) == True:
     print("USING CUSTOM PRODUCTS CSV FILE...")
-    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
+    csv_filepath = read_csv("products.csv")
 else:
     print("USING DEFAULT PRODUCTS CSV FILE...")
-    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "default_products.csv")
+    csv_filepath = read_csv( "default_products.csv")
 
 
 
@@ -34,12 +35,10 @@ print("---------")
 print("THERE ARE", len(products), "PRODUCTS:")
 print("---------")
 
-for p in products:
-    print("..." + p["name"] + "   " + to_usd(p["price"]))
-
 
 all_prices = []
 for p in products:
+    print("..." + p["name"] + "   " + to_usd(p["price"]))
     all_prices.append(float(p["price"]))
 
 import statistics
